94b5b8f20673170127165cb3d58288d1e82a2bf7
marshall8009 <marshall_8009@yahoo.com>
2025-06-23 15:02:26 -0500

    added budget item logic for adding and deleting items

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AddItemForm } from '../add-item-form/add-item-form';
 import { BudgetItemList } from '../budget-item-list/budget-item-list';
+import { BudgetItem } from '../../shared/models/budget-item.model';
 
 @Component({
   selector: 'app-main-page',
@@ -9,5 +10,14 @@ import { BudgetItemList } from '../budget-item-list/budget-item-list';
   styleUrl: './main-page.component.scss'
 })
 export class MainPage {
+  budgetItems: BudgetItem[] = new Array<BudgetItem>();
 
+  addItem(newItem: BudgetItem) {
+    this.budgetItems.push(newItem);
+  }
+
+  deleteItem(item: BudgetItem) {
+    let index = this.budgetItems.indexOf(item);
+    this.budgetItems.splice(index, 1);
+  }
 }
